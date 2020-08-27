257a2ba94a2f5300807f689f3cf018531d1198a2
Waxaranai <wisnuaa43@gmail.com>
2020-08-27 19:40:24 +0800

    test(AkairoHandler): test expression

--- src/struct/AkairoHandler.js
+++ src/struct/AkairoHandler.js
@@ -83,7 +83,7 @@ class AkairoHandler extends EventEmitter {
         mod.filepath = filepath;
         mod.client = this.client;
         mod.handler = this;
-        if (mod instanceof Command && this.defaultChannel) mod.channel = mod.channel === undefined ? undefined : this.defaultChannel ? this.defaultChannel : mod.channel;
+        if (mod instanceof Command && this.defaultChannel) mod.channel = mod.channel ? mod.channel : !mod.channel ? undefined : this.defaultChannel;
 
         this.modules.set(mod.id, mod);
 
